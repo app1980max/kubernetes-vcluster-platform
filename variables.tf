c7fe57461c85785b7a1d3085ed9d9b0fbab63cd4
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-18 00:20:29 +0300

    update

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "kubeconfig_path" {
   description = "Path to kubeconfig for the target cluster"
   default     = "~/.kube/config"
 }
+
+variable "vclusters" {
+  description = "Mapping of vCluster logical names to internal identifiers"
+  type        = map(string)
+  default     = {}  # optional, can be empty
+}
